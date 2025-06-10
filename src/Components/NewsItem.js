a360f2363253dfd77344b664d13f617c15686e66
code_bhartisuhani <156918664+SuhaniBharti@users.noreply.github.com>
2025-06-10 20:26:57 +0530

    Updated NewsItem.js

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,29 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const NewsItem=(props)=> {
-   
- 
-    let{title,description,imageUrl,newsUrl,author,time,source}=props;
+export class NewsItem extends Component {
+  render() {
+    let{title,description,imageUrl,newsUrl} =this.props;
     return (
+        
       <div className='my-3'>
-        <div className="card" style={{width: "18rem"}}>
-          <div>
-          <span class=" badge rounded-pill text-bg-danger " >{source}</span>
-
-        </div>
-
-<img src={!imageUrl?"https://cdn.wionews.com/sites/default/files/2024/06/23/439529-warp-drives.png":imageUrl}
- className="card-img-top" alt="..."/>
-<div className="card-body">
-<h5 className="card-title">{title}</h5>
-<p className="card-text">{description}</p>
-<p className="card-text"><small class="text-muted">By {!author?"Unknown":author} on {new Date(time).toGMTString()}</small></p>
-<a href={newsUrl} target="blank" className="btn btn-sn btn-dark">read more</a>
+        <div className="card" >
+  <img src={imageUrl} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}...</h5>
+    <p className="card-text">{description}...</p>
+    <a href={newsUrl}target='_blank' rel="noreferrer" className="btn btn-sm btn-dark btn-primary">read more</a>
+  </div>
 </div>
-</div>
-</div>
+      </div>
     )
-  
+  }
 }
 
 export default NewsItem
